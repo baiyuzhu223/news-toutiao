fd2e7b5c38656c0dda5a99a9b547019e4e23f9f8
baiyuzhu223 <1944141116@qq.com>
2019-10-28 21:52:45 +0800

    登录进主页

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,22 @@ import VueRooter from 'vue-router'
 import Vue from 'vue'
 
 import Login from '@/views/login'
-
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 Vue.use(VueRooter)
 
 const router = new VueRooter({
   routes: [
-    { path: '/login', component: Login }
+    // 登录
+    { path: '/login', component: Login },
+    // 首页
+    { path: '/',
+      component: Home,
+      children: [
+        { path: '/', component: Welcome }
+      ] },
+    { path: '*', component: NotFound }
   ]
 })
 export default router
